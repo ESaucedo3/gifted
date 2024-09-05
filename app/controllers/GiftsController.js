8c7944fda4993fb42c77471bc7efa518750829d8
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-09-05 17:00:00 -0600

    updates

--- app/controllers/GiftsController.js
+++ app/controllers/GiftsController.js
@@ -1,7 +1,8 @@
-import { AppState } from '../AppState.js';
-import { giftsService } from '../services/GiftsService.js';
-import { Pop } from '../utils/Pop.js';
-import { setHTML } from '../utils/Writer.js';
+import {AppState} from '../AppState.js';
+import {giftsService} from '../services/GiftsService.js';
+import {getFormData} from '../utils/FormHandler.js';
+import {Pop} from '../utils/Pop.js';
+import {setHTML} from '../utils/Writer.js';
 
 export class GiftsController {
   constructor() {
@@ -19,15 +20,36 @@ export class GiftsController {
     }
   }
 
-  async openGift(giftId) {
+  async createGift() {
     try {
-      await giftsService.openGift(giftId)
-
+      event.preventDefault();
+      const form = event.target;
+      const giftFormData = getFormData(form);
+      await giftsService.createGift(giftFormData);
     } catch (e) {
       Pop.error(e);
       console.error(e);
     }
+  }
 
+  async openGift(giftId) {
+    try {
+      await giftsService.openGift(giftId);
+    } catch (e) {
+      Pop.error(e);
+      console.error(e);
+    }
+  }
+
+  async searchGif() {
+    try {
+      // @ts-ignore
+      const result = document.getElementById('result').value;
+      await giftsService.searchGif(result);
+    } catch (e) {
+      Pop.error(e);
+      console.error(e);
+    }
   }
 
   drawGifts() {
--- app/services/GiftsService.js
+++ app/services/GiftsService.js
@@ -1,8 +1,16 @@
-import { AppState } from '../AppState.js';
-import { Gift } from '../models/Gift.js';
-import { api } from './AxiosService.js';
+import {AppState} from '../AppState.js';
+import {Gift} from '../models/Gift.js';
+import {api, giphyApi} from './AxiosService.js';
 
 class GiftsService {
+  async searchGif(result) {
+    const response = await giphyApi.get;
+  }
+  async createGift(giftData) {
+    const response = await api.post('api/gifts', giftData);
+    const newGift = new Gift(response.data);
+    AppState.gifts.push(newGift);
+  }
   async getGifts() {
     const response = await api.get('api/gifts');
     console.log(response.data);
@@ -10,17 +18,15 @@ class GiftsService {
     AppState.gifts = gifts;
   }
 
-
   async openGift(giftId) {
-    const gifts = AppState.gifts
-    const giftIndex = gifts.findIndex(gift => giftId === gift.id)
-    const gift = gifts[giftIndex]
-    const giftdata = { opened: !gift.opened }
-    const response = await api.put(`api/gifts/${giftId}`, giftdata)
+    const gifts = AppState.gifts;
+    const giftIndex = gifts.findIndex((gift) => giftId === gift.id);
+    const gift = gifts[giftIndex];
+    const giftdata = {opened: !gift.opened};
+    const response = await api.put(`api/gifts/${giftId}`, giftdata);
     const updatedgift = new Gift(response.data);
-    gifts.splice(giftIndex, 1, updatedgift)
-    console.log(gifts, response.data)
-
+    gifts.splice(giftIndex, 1, updatedgift);
+    console.log(gifts, response.data);
   }
 }
 
